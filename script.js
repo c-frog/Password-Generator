fca4d9227ffc85e07f469f3a1bf48b16d8b3c659
Clyde Plasencia <cquiatt@gmail.com>
2019-09-24 20:42:16 -0600

    initial file creations, with boilerplate html and css

---
+++ script.js
@@ -0,0 +1,14 @@
+// generate random password
+function generate(){
+    //set password length
+    var passwordLength = prompt ('Choose a length between 8 and 120')
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
+    var userChar = prompt ("Would you like letters, numbers, special characters, or a mix?")
+
+        if 
+
+        
+
+}
+
+generate()
